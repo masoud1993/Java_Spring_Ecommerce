6b7ba95ef082b5d2ac716969f8795a7787f68b5a
m.badrmiran <masoud.badremiran@gmail.com>
2026-06-06 17:04:00 +0330

    lombok added

--- src/main/java/org/masa/ecom/service/CategoryServiceImpl.java
+++ src/main/java/org/masa/ecom/service/CategoryServiceImpl.java
@@ -2,7 +2,6 @@ package org.masa.ecom.service;
 
 import org.masa.ecom.model.Category;
 import org.masa.ecom.repository.CategoryRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
